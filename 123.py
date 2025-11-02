82c501a0e847ef0997a76231472c06987318382f
Ali K Omran <AO2400005@tkh.edu.eg>
2025-11-02 09:57:58 +0200

    added 123.py

---
+++ 123.py
@@ -0,0 +1,12 @@
+name = "Ali"
+time = "11 Am"
+date = "November 2"
+age = "20"
+snack = "Pizza"
+
+print(f"Name: {name}")
+print(f"Current Time: {time}")
+print(f"Date: {date}")
+print(f"Age: {age}")
+print(f"Favorite Snack: {snack}")
+print(f"\nHello my name is {name}!, I am {age} years old, it's {time} on {date}, and I am enjoying some {snack}! ")
